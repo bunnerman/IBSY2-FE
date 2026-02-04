222475aec8a224285e530d075f3517dcdfbdf76e
bunnerman <parnilrbalawade@gmail.com>
2026-02-04 20:43:24 +0530

    Create Objectives.jsx

---
+++ 3 Bootstrap/Objectives.jsx
@@ -0,0 +1,31 @@
+import '../index.css'
+
+function Objectives() {
+	return (
+		<div id="objectives" className="m-2 p-2 border rounded">
+				<h3>Objectives of Experiment</h3>
+				<ol>
+					<li>
+						Integrate Bootstrap into a React project. Understand the basics of
+						Bootstrap classes for styling. Verify the Bootstrap integration by
+						applying styles to components.
+					</li>
+
+					<li>
+						Explore the Bootstrap grid system for responsive layouts. Implement a
+						responsive design using Bootstrap classes. Test the layout on various
+						screen sizes for consistency.
+					</li>
+
+					<li>
+						Utilize essential Bootstrap components such as navigation bars, buttons,
+						and forms. Customize and style Bootstrap components to match the
+						application's design. Ensure proper functionality and styling of
+						Bootstrap components.
+					</li>
+				</ol>
+			</div>
+	);
+}
+
+export default Objectives
