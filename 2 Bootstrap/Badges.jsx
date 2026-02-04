a595438c9c05bfd4fb24e4999769bdc98c42f6fa
bunnerman <parnilrbalawade@gmail.com>
2026-02-04 16:35:04 +0530

    Create Badges.jsx

---
+++ 2 Bootstrap/Badges.jsx
@@ -0,0 +1,14 @@
+import '../index.css'
+
+function Badges() {
+	return (
+		<>
+		<span class="badge text-bg-success" style={{marginLeft: 5}}>Success</span>
+		<span class="badge text-bg-warning" style={{marginLeft: 5}}>Warning</span>
+		<span class="badge text-bg-danger" style={{marginLeft: 5}}>Danger</span>
+		<span class="badge text-bg-info" style={{marginLeft: 5}}>Info</span>
+		</>						
+	);
+}
+
+export default Badges;
