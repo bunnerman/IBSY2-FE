cde93e2585813a1bc3c76e649f39e6e11371d271
bunnerman <parnilrbalawade@gmail.com>
2026-02-09 20:51:54 +0530

    Create App.js

---
+++ 4 React/App.js
@@ -0,0 +1,14 @@
+import './index.css';
+import CustomForm from './components/CustomForm.js'
+
+function App() {
+  return (
+    <div style={{minHeight: "100vh", padding: 15}}>
+
+			<CustomForm></CustomForm>
+
+    </div>
+  );
+}
+
+export default App
