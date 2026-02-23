14d1fc0c8471ec3f0e4847a78513ce72da5e2e75
bunnerman <parnilrbalawade@gmail.com>
2026-02-23 20:33:05 +0530

    Create App.js

---
+++ 4A React useState useEffect/App.js
@@ -0,0 +1,13 @@
+import './index.css';
+import Compo from './components/Compo.js'
+import {useState, useEffect} from 'react'
+
+function App() {
+  return (
+    <div style={{minHeight: "100vh"}}>
+			<Compo></Compo>
+    </div>
+  )
+}
+
+export default App
