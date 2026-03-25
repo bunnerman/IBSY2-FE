ca7544b351009d1efbe7bc21f3685109a3b918f8
bunnerman <parnilrbalawade@gmail.com>
2026-03-25 21:41:29 +0530

    Update errorWrapper.js

--- 5C Error Boundaries/errorWrapper.js
+++ 5C Error Boundaries/errorWrapper.js
@@ -1,6 +1,7 @@
 import "../index.css";
 import ErrorBoundary from "./ErrorBoundary.js";
 
+// wraps each component in error boundary, for individual error boundary returning instead of whole page crashing at once
 function errorWrapper(Component) 
 {
   return function(props) 
