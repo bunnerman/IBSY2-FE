4f7c081ee659d9e0f7cecf8bc71988774f9d7847
bunnerman <parnilrbalawade@gmail.com>
2026-03-25 22:36:44 +0530

    Update ErrorBoundary.js

--- 5C Error Boundaries/ErrorBoundary.js
+++ 5C Error Boundaries/ErrorBoundary.js
@@ -9,6 +9,9 @@ class ErrorBoundary extends React.Component {
   static getDerivedStateFromError(error) {
     return { hasError: true };
   }
+	componentDidCatch(error, info) {
+		console.log(error, info);
+	}
 
   render() {
     if (this.state.hasError)
