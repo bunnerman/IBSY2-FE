9616b46a70c749f026d2deea3cdab116c367420f
bunnerman <parnilrbalawade@gmail.com>
2026-03-26 19:41:51 +0530

    withdrawed the WITHDRAW

--- 6 Redux/App.js
+++ 6 Redux/App.js
@@ -11,7 +11,7 @@ function App()
 		<div className = "customTheme" style = {{ minHeight: "100vh", borderStyle: "none", margin: "0", textAlign: "center"}}>
 			<h2>Balance: {balance}</h2>
 			<button className = "depositBtn" onClick={()=> dispatch({type: "DEPOSIT"})}>Deposit ($100)</button>
-			<button className = "withdrawBtn" onClick={()=> dispatch({type: "WITHDRAW"})}>WITHDRAW ($50)</button>
+			<button className = "withdrawBtn" onClick={()=> dispatch({type: "WITHDRAW"})}>Withdraw ($50)</button>
 		</div>
 	)
 }
