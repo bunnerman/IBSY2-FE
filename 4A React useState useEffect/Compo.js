c8522e590bbfa9cf282876f2c5586d7e9d5d0f25
bunnerman <parnilrbalawade@gmail.com>
2026-04-22 22:20:10 +0530

    Fix decrement button spacing in Compo.js

--- 4A React useState useEffect/Compo.js
+++ 4A React useState useEffect/Compo.js
@@ -12,7 +12,7 @@ function Compo() {
 			<h1 style={{textAlign: "center"}}>Count: {count}</h1>
 			<div className="text-center">
 				<button type="button" className="btn btn-lg btn-outline-success m-1" onClick={()=>setCount(count + 1)}>Increment</button>
-				<button type="button" className="btn btn-lg btn-outline-danger m-1" onClick={()=>setCount(count -1)}>Decrement</button>
+				<button type="button" className="btn btn-lg btn-outline-danger m-1" onClick={()=>setCount(count - 1)}>Decrement</button>
 				<button type="button" className="btn btn-lg btn-outline-secondary m-1" onClick={()=>setCount(0)}>Reset</button>
 				<br/>
 				<button type="button" className="btn btn-lg btn-success m-1" onClick={()=>setCount(count * 2)}>x2</button>
